3be9cff3026c5ce8589327fefdc8db44f3c9219b
Daniil <zolotovdaniil2@gmail.com>
2021-07-29 20:27:55 +0300

    Add animation

---
+++ js/main.js
@@ -0,0 +1,70 @@
+var btn = document.querySelector('.btn')
+btn.onclick= function (){
+alert('Ну как Вам моя вёрстка?')
+}
+$(function () {
+
+	$(window).scroll(function() {
+	    $('.future').each(function(){
+	        var imagePos = $(this).offset().top;
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('.icon1').each(function(){
+	        var imagePos = $(this).offset().top;
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('.icon2').each(function(){
+	        var imagePos = $(this).offset().top;
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('.icon3').each(function(){
+	        var imagePos = $(this).offset().top;
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('form').each(function(){
+	        var imagePos = $(this).offset().top;
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('h2').each(function(){
+	        var imagePos = $(this).offset().top;
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('p').each(function(){
+	        var imagePos = $(this).offset().top;
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+})
